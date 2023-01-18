8102c2394bef8bec14820591a01f4f325c90d334
Ruslan <amachaevr@gmail.com>
2023-01-18 14:08:47 +0200

    add img, main files html 'licevaia', first app 'crm'

--- Otel/settings.py
+++ Otel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crm.apps.CrmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Otel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ crm/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Order(models.Model):
+    order_dt = models.DateTimeField(auto_now=True)
+    order_name = models.CharField(max_length=50, verbose_name='Имя')
+    order_surname = models.CharField(max_length=50, verbose_name='Фамилия')
+    order_international = models.CharField(max_length=50, verbose_name='Гражданство')
+    order_phone = models.CharField(max_length=50, verbose_name='Телефон')
+    order_mail = models.CharField(max_length=50, verbose_name='Мэйл')
+    order_country = models.CharField(max_length=50, verbose_name='Страна куда хочет')
+    order_CV = models.CharField(max_length=50, verbose_name='Его CV')
+    order_vakansia = models.CharField(max_length=50, verbose_name='На какую вакансию')
+
